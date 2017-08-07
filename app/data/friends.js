dd3e5a8867c2b85c9aa52383048d2e5f85035ed4
Dil2000 <dnks2000@gmail.com>
2017-08-06 20:19:58 -0400

    all

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,7 +14,87 @@ var friends = [{
       4,
       1
     ]
-}
+},
+{
+  "name": "Maisie Williams",
+  "photo": "http://vignette3.wikia.nocookie.net/gameofthrones/images/0/0f/Arya_stark_S5.jpg/revision/latest?cb=20150311174943",
+  "scores": [
+      5,
+      1,
+      2,
+      5,
+      5,
+      4,
+      3,
+      3,
+      5,
+      4
+  ]
+  },
+  {
+  "name": "Benedict Cumberbatch",
+  "photo": "http://i1.mirror.co.uk/incoming/article3170930.ece/ALTERNATES/s615b/Sherlock-Holmes-Benedict-Cumberbatch.jpg",
+  "scores": [
+      5,
+      2,
+      4,
+      5,
+      2,
+      3,
+      2,
+      1,
+      5,
+      4
+  ]
+  },
+{
+  "name": "scooby doo",
+  "photo": "https://www.wbkidsgo.com/Portals/4/Images/Content/Characters/Scooby/scooby-SD_headShot.png?ver=2015-05-15-175406-203",
+  "scores": [
+      2,
+      5,
+      2,
+      2,
+      5,
+      1,
+      4,
+      2,
+      5,
+      5
+  ]
+  },
+  {
+  "name": "Emilia Clarke",
+  "photo": "https://vignette1.wikia.nocookie.net/disney/images/f/f9/Emilia_Clarke.jpg/revision/latest?cb=20161119001548",
+  "scores": [
+      2,
+      5,
+      2,
+      5,
+      2,
+      1,
+      1,
+      3,
+      5,
+      2
+]
+},
+{
+  "name": "Jim Parsons",
+  "photo": "http://pixel.nymag.com/imgs/daily/vulture/2014/05/02/magazine/02-jim-parsons-2.nocrop.w529.h746.jpg",
+  "scores": [
+      1,
+      5,
+      4,
+      4,
+      2,
+      2,
+      3,
+      2,
+      4,
+      1
+  ]
+  }
 ];
 
 module.exports = friends;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 // Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
@@ -15,7 +16,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-
 // Style Sheets and images
 app.use(express.static(__dirname + "/css"));
 app.use(express.static(__dirname + "/images"));
@@ -31,10 +31,6 @@ app.get("/survey", function(req, res) {
 
 
 
-
-
-
-
 var friendChoices = require("./app/data/friends");
 
 // Get Json data from json
@@ -43,18 +39,55 @@ app.get("/api/friends",function(req,res){
 });
 
 app.post("/api/friends", function(req, res) {
-	// var newFriend = {
- //      name: "abra kadabra",
- //      photo: "djkjfkds",
- //      scores: [1,2,3,4,5,1,2,3,4,5]
- //    }
+
  	var newFriend = req.body;
-
 	friendChoices.push(newFriend);
-	res.json(newFriend);
-	console.log("Add");
 
+	//res.json(newFriend);
+	console.log("Added new details");
+
+	var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: Infinity
+    };
+
+    // Here we take the result of the user"s survey POST and parse it.
+    var userData = req.body;
+    var userScores = userData.scores;
+
+    // This variable will calculate the difference between the user"s scores and the scores of
+    // each user in the database
+    var totalDifference;
+    console.log("user Data : " + userData.name);
+
+    // Here we loop through all the friend possibilities in the database.
+    for (var i = 0; i < (friendChoices.length -1) ; i++) {
+      var currentFriend = friendChoices[i];
+      totalDifference = 0;
+
+      // We then loop through all the scores of each friend
+      for (var j = 0; j < currentFriend.scores.length; j++) {
+        var currentFriendScore = currentFriend.scores[j];
+        var currentUserScore = userScores[j];
+
+        // We calculate the difference between the scores and sum them into the totalDifference
+        totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+      }
+
+      // If the sum of differences is less then the differences of the current "best match"
+      if (totalDifference <= bestMatch.friendDifference) {
+        // Reset the bestMatch to be the new friend.
+        bestMatch.name = currentFriend.name;
+        bestMatch.photo = currentFriend.photo;
+        bestMatch.friendDifference = totalDifference;
+        console.log("best match : " + bestMatch.name + "   " + bestMatch.friendDifference);
+      }
+
+    }
+    res.json(bestMatch);
 });
+
 
 
 app.listen(PORT, function() {
